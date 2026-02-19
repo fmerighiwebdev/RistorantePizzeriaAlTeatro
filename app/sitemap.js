fcea9c4f9548926ae654ffd9d4acc87242aaad27
Francesco Merighi <51082657+fmerighiwebdev@users.noreply.github.com>
2026-02-19 01:07:40 +0100

    robots and sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,30 @@
+export default function sitemap() {
+  const baseUrl = 'https://ristorantealteatro.it'
+
+  return [
+    {
+      url: `${baseUrl}/`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/menu`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/booking`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+    {
+      url: `${baseUrl}/privacy-policy`,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.3,
+    },
+  ]
+}
